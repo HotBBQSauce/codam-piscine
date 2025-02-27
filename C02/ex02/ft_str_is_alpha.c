6ff51a86a2c6d9563955566d01b548c7f1749e6f
HotBBQSauce <hotbbqsauce@mailbox.org>
2025-02-27 17:08:13 +0100

    Fixed ex02

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: dguliev <dguliev@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/26 14:17:02 by dguliev       #+#    #+#                 */
-/*   Updated: 2025/02/27 15:16:09 by dguliev        ########   odam.nl        */
+/*   Updated: 2025/02/27 17:07:51 by dguliev        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	ft_str_is_alpha(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (!(str[i] >= 'a' && str[i] <= 'z')
-			|| (str[i] >= 'A' && str[i] <= 'Z'))
+		if (!((str[i] >= 'a' && str[i] <= 'z')
+				|| (str[i] >= 'A' && str[i] <= 'Z')))
 		{
 			return (0);
 		}
@@ -28,18 +28,17 @@ int	ft_str_is_alpha(char *str)
 	}
 	return (1);
 }
+
 // int	main(void)
 // {
 // 	int	result;
-// 	char string[] = "yolo1";
-//
+// 	char string[] = "YoLo";
 // 	result = ft_str_is_alpha(string);
-//
-// 	if(result)
+// 	if(result == 1)
 // 	{
-// 		printf("%s has nummeric value.", string);
+// 		printf("%s only alphabetic.", string);
 // 		return (2);
 // 	}
-// 	printf("%s has no nummeric value.", string);
+// 	printf("%s has other values.", string);
 // 	return (2);
 // }

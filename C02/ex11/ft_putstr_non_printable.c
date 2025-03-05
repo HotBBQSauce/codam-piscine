44e96335c3f41be0b1fbfed51617ae2ae8f07b5d
HotBBQSauce <hotbbqsauce@mailbox.org>
2025-03-05 14:34:13 +0100

    C02 ex11 improved

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_putstr_non_printable.c                           :+:    :+:           */
+/*   ft_putstr_non_printable.c                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: dguliev <dguliev@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/04 13:07:34 by dguliev       #+#    #+#                 */
-/*   Updated: 2025/03/05 13:41:48 by dguliev        ########   odam.nl        */
+/*   Updated: 2025/03/05 14:32:39 by dguliev       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,22 @@
 
 void	char_to_hex(char c, char hex_str[3])
 {
-	int	high_nibble;
-	int	lower_nibble;
+	char	*base;
 
-	high_nibble = (c >> 4) & 0xF;
-	lower_nibble = c & 0xF;
-	if (high_nibble < 10)
-		hex_str[0] = high_nibble + '0';
-	else
-		hex_str[0] = high_nibble - 10 + 'a';
-	if (lower_nibble < 10)
-		hex_str[0] = lower_nibble + '0';
-	else
-		hex_str[0] = lower_nibble - 10 + 'a';
+	base = "0123456789abcdef";
+	hex_str[0] = base[c / 16];
+	hex_str[1] = base[c % 16];
 	hex_str[2] = '\0';
 	return ;
 }
 
 void	write_hex(int count, char hex_str[3])
 {
+	write(1, "\\", 1);
 	while (hex_str[count])
 	{
-		if (count == 0)
-			write(1, "\\", 1);
-		if (hex_str[count])
-		{
-			write(1, (&hex_str[count]), 1);
-			count++;
-		}
+		write(1, (&hex_str[count]), 1);
+		count++;
 	}
 }
 

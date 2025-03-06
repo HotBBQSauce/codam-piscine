a2e98bde22f879d65cfc87bb476f24f058190b86
HotBBQSauce <hotbbqsauce@mailbox.org>
2025-03-06 18:15:17 +0100

    C04/ex03 - ft_atoi added

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,16 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                         ::::::::           */
-/*   ft_atoi.c                                           :+:    :+:           */
-/*                                                      +:+                   */
-/*   By: dguliev <marvin@42.fr>                        +#+                    */
-/*                                                    +#+                     */
-/*   Created: 2025/03/03 17:35:54 by dguliev        #+#    #+#                */
-/*   Updated: 2025/03/03 17:39:59 by dguliev        ########   odam.nl        */
+/*                                                        ::::::::            */
+/*   ft_atoi.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dguliev <marvin@42.fr>                       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/03/03 17:35:54 by dguliev       #+#    #+#                 */
+/*   Updated: 2025/03/06 14:26:17 by dguliev       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(char *str)
 {
-	
+	int	count_negative;
+	int	i;
+	int	result;
+
+	count_negative = 0;
+	i = 0;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			count_negative++;
+		i++;
+	}
+
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if (count_negative % 2 != 0)
+		result *= -1;
+	return (result);
 }
+
